3b06796339561bd72d6444679ef8143271caceec
Yuva <yuva@heroic.us>
2024-12-04 01:23:38 +0000

    3 part 2

--- 3.py
+++ 3.py
@@ -14,17 +14,24 @@ def part1():
 print(part1())
 
 
-
+#duh! took so long to learn that each line cannot be processed by itself, its all one line
 def part2():
     total =0
-    with open("3.txt") as file:
-        for line in file:
-            # print(line)
-            matches = re.findall(r'mul\((\d+),(\d+)\)', line)
+    parts  = re.split(r'(don\'t\(\)|do\(\))', open("3.txt").read())
+    skipTillDo = False
+    for part in parts:
+        if part == 'don\'t()':
+            skipTillDo = True
+        elif part =='do()':
+            skipTillDo = False
+        else:
+            if(skipTillDo):
+                continue
+            matches = re.findall(r'mul\((\d+),(\d+)\)', part)
             for match in matches:
                 result = int(match[0]) * int(match[1])
                 total += result
     return total
 
-print(part1())
+print(part2())
 
