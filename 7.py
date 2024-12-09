63959a74ad518b57633fde0e1ed6ede1e807df7f
Yuva <yuva@heroic.us>
2024-12-09 22:54:18 +0000

    make code clear for part 2

--- 7.py
+++ 7.py
@@ -1,9 +1,11 @@
 import re
 
-def is_valid(value, numbers):
+def is_valid(value, numbers, part2):
     if(len(numbers) == 2):
-        if (value == numbers[0] * numbers[1]) or (value == numbers[0] + numbers[1]) or (value == int(f"{numbers[0]}{numbers[1]}")):
+        if (value == numbers[0] * numbers[1]) or (value == numbers[0] + numbers[1]):
             return True
+        if part2:
+            return (value == int(f"{numbers[0]}{numbers[1]}"))
         return False
     elif(len(numbers) > 2):
         mul = numbers[0] * numbers[1]
@@ -16,20 +18,23 @@ def is_valid(value, numbers):
             list1.append(numbers[i])
             list2.append(numbers[i])
             list3.append(numbers[i])
-        return (is_valid(value, list1) or is_valid(value, list2) or is_valid(value, list3))
+        if(part2):  
+            return (is_valid(value, list1, True) or is_valid(value, list2, True) or is_valid(value, list3, True))
+        return (is_valid(value, list1, False) or is_valid(value, list2, False))
         
     else:
         return False    
 
-def part2():
+def part(part2):
     with open("7.txt") as file:
         input = [(line.rstrip()) for line in file]
         total = 0
         for calc in input:
             value = int(calc.split(':')[0])
             numbers =  [int(x) for x in calc.split(':')[1].split()]
-            if is_valid(value, numbers):
+            if is_valid(value, numbers, part2):
                 total +=value
         return total
 
-print(part2())
+print(part(False))
+print(part(True))
