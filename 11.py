9fb3eb046b4aabf4109f623a24112cd71bf46cb9
Yuva <yuva@heroic.us>
2024-12-22 19:16:04 +0000

    figured I need to do the iteration for one item at a time but then I had to look up  clues to use lru_cache..

--- 11.py
+++ 11.py
@@ -1,33 +1,35 @@
+from functools import lru_cache
 import re
 parts  = [int(p) for p in re.split(r'\s+', open("11.txt").read())]
 
 print(parts)
 
-def blink_once(input):
+@lru_cache
+def blink_once(number):
     output = []
-    for number in input:
-        str_num = str(abs(number))
-        if number == 0:
-            output.append(1)
-        elif (len(str_num) % 2) == 0:
-            mid = int(len(str_num)/2)
-            output.append(int(str_num[:mid]))
-            output.append(int(str_num[mid:]))
-        else:
-            output.append(number * 2024)
-    # print(output)
+    str_num = str(abs(number))
+    if number == 0:
+        output.append(1)
+    elif (len(str_num) % 2) == 0:
+        mid = int(len(str_num)/2)
+        output.append(int(str_num[:mid]))
+        output.append(int(str_num[mid:]))
+    else:
+        output.append(number * 2024)
     return output
 
-def blink(input, max_blink):
-    output = None
-    while max_blink > 0:
-        output = blink_once(input)
-        input = output
-        max_blink -= 1
-    return output
+@lru_cache(None)
+def blink(number, max_blink):
+    if(max_blink == 1):
+        return len(blink_once(number))
+    return sum([blink(out_item, max_blink - 1) for out_item in blink_once(number)])
+
 
 def part1(parts):
-    output = blink(parts, 25)
-    print (len(output))
+    count = 0
+    for part in parts:
+        count += blink(part, 75)
+   
+    print (count)
 
 part1(parts)
